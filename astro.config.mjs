efef6cfedf049f16144344936b4b94612b7303bb
Mukesh <er.mks89@gmail.com>
2026-07-04 12:16:55 +0530

    config: configure site URL for mukeshsahnis.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://onesmiledentalcare.com/",
+  site: "https://mukeshsahnis.github.io",
   vite: {
     plugins: [tailwindcss()]
   },
